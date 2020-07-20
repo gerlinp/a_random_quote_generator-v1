105e76b9187fe7318cdc27be90d9759901020518
Gerlinp <gerlin.pierrelouis@gmail.com>
2020-07-20 01:12:55 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,12 +26,14 @@ const quotes = [
   },
 
   { quote:"I don't like two-legged things.",
-  source:'Red XIII / Nanaki'
+  source:'Red XIII / Nanaki',
+  system:'Playstation'
   },
 
   {quote:"I always thought this planet was so huge. But lookin' at it from space, I realized it's so small. We're just floatin' in the dark. ......kinda makes you feel powerless. On top of that it's got Sephiroth festerin' inside it like a sickness. That's why I say this planet's still a kid. A little kid sick and trembling in the middle of this huge universe. Someone's gotta protect it. Ya follow me? That someone is us.",
   source:"Cid Higwind",
-  year:'1997'},
+  year:'1997',
+  system:'Playstation'},
 
   {quote:"Cloud...sign this. It's a contract that says when the war is over, all the materia will belong to me.",
   source:"Yuffie Kisaragi",
@@ -43,9 +45,20 @@ const quotes = [
   }
 ]
 
+
+// RGB function based off the refractor challenge  from the Java Loop Lesson
+const randomValue = () => Math.floor(Math.random() * 256);
+
+function randomRGB(value) {
+  const color = `rgb(${randomValue()}, ${randomValue()}, ${randomValue()} )`;
+  return color;
+}
+
 /***
  * `getRandomQuote` function
 ***/
+// based on random number function used throughout the course
+
 function getRandomQuote() {
   const randomNumber = Math.floor(Math.random() * quotes.length)
   return randomNumber
@@ -55,6 +68,7 @@ getRandomQuote()
 /***
  * `printQuote` function
 ***/
+
 function printQuote() {
   let randomQuote = getRandomQuote()
   let displayQuote = `<p class="quote">${quotes[randomQuote].quote}`
@@ -65,8 +79,12 @@ function printQuote() {
   if ('year' in quotes[randomQuote]) {
     displayQuote += `<span class="year">${quotes[randomQuote].year}</span>`
   }
+  if ('system' in quotes[randomQuote]) {
+    displayQuote += `<span class="system">, ${quotes[randomQuote].system}</span>`
+  }
   displayQuote += `</p>`
   document.getElementById('quote-box').innerHTML = displayQuote;
+  document.body.style.background = randomRGB(randomValue)
 }
 
 /***
@@ -75,3 +93,4 @@ function printQuote() {
 ***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
+setInterval(printQuote,10000);
